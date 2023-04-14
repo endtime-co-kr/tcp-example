159d5ca65e64bba1f5dfe82fe94a776e215b6730
seokhun <seokhun.yun1@gmail.com>
2023-04-14 14:23:36 +0900

    tcp example ver 1

---
+++ TcpClientExample/Program.cs
@@ -0,0 +1,21 @@
+﻿using TcpClientExample.etTcp;
+
+namespace TcpClientExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            AsyncTcp tcp = new AsyncTcp();
+            tcp.Connect("192.168.123.97", 4001);
+
+            while (true)
+            {
+                var t = Console.ReadLine();
+                tcp.Send(t + "\r\n");
+            }
+        }
+    }
+}
---
+++ TcpClientExample/etTcp/AsyncTcp.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using static TcpClientExample.etTcp.AsyncTcp;
+
+namespace TcpClientExample.etTcp
+{
+    public class AsyncTcp
+    {
+        private ManualResetEvent _connEvt = new ManualResetEvent(false);
+        private bool _isConnected = false;
+        private TcpClient _tcpClient = new TcpClient();
+        private byte[] _recvBuffer= new byte[256];
+
+        public bool Connect(string host, int port)
+        {
+            IPAddress.TryParse(host, out IPAddress? ipAddress);
+            if (ipAddress == null)
+            {
+                return false;
+            }
+
+            _tcpClient = new TcpClient();
+            _tcpClient.BeginConnect(host, port, new AsyncCallback(ConnectCallback), _tcpClient);
+            _connEvt.WaitOne(5000);
+            if (_isConnected)
+            {
+                Task.Run(async () => {
+                    while (true)
+                    {
+                        await ReadLinesAsync(_tcpClient);
+                    }
+                    });
+                return true;
+            }
+            return false;
+        }
+
+        public void ConnectCallback(IAsyncResult ar)
+        {
+            try
+            {
+                TcpClient client = (TcpClient)ar.AsyncState;
+                if (client == null)
+                {
+                    return;
+                }
+                client.EndConnect(ar);
+                Console.WriteLine("connected to {0}",
+                    client.Client.RemoteEndPoint?.ToString());
+                _isConnected = true;
+                _connEvt.Set();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+
+
+        public async Task<string> ReadLinesAsync(TcpClient client)
+        {            
+            int readResult = await _tcpClient.GetStream().ReadAsync(_recvBuffer, 0, _recvBuffer.Length);
+            Array.Resize(ref _recvBuffer, readResult);
+            string recvString = Encoding.ASCII.GetString(_recvBuffer);
+            Console.Write(recvString);
+            return recvString;
+        }
+
+
+        public void Send(string data)
+        {
+            byte[] byteData = Encoding.ASCII.GetBytes(data);
+            _tcpClient.Client.BeginSend(byteData, 0, byteData.Length, 0,
+                new AsyncCallback(SendCallback), _tcpClient);
+        }
+
+        private void SendCallback(IAsyncResult ar)
+        {
+            try
+            {
+                TcpClient client = (TcpClient)ar.AsyncState;
+                int bytesSent = client.Client.EndSend(ar);
+                Console.WriteLine("Sent {0} bytes to server.", bytesSent);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
